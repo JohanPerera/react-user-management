3b8ab6685123c0ff74f508cd06c30b7b5b2394e3
Johan Perera <johanpriyasanka@gmail.com>
2021-09-10 15:43:28 +0530

    CRUD works

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,19 @@
+import './Form.css';
 import React, { Component } from "react";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
 import ToggleButton from "@material-ui/lab/ToggleButton";
 import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormLabel from '@material-ui/core/FormLabel';
+import Select from '@material-ui/core/Select';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+
 
 export class Form extends Component {
     //   static updateMode = true;
@@ -11,20 +22,25 @@ export class Form extends Component {
         super(props);
 
         this.state = {
+            _id: "",
             username: "",
             address: "",
             phonenumber: "",
             sex: "female",
             updateMode: true,
-           users:[]
+            users: []
         };
 
-       
 
 
+
+
+    }
+
+    componentWillMount() {
         axios.get('http://localhost:3001/users').then((res) => {
             if (res && res.data) {
-                const users =  res.data;
+                const users = res.data;
                 this.setState({
                     users
                 })
@@ -36,7 +52,7 @@ export class Form extends Component {
         )
     }
 
-    
+
 
     handleUsernameChange = (event) => {
         this.setState({
@@ -82,17 +98,14 @@ export class Form extends Component {
             });
     };
 
-    handleDelete = (event) => {
-        event.preventDefault();
-        console.log("delete");
-    };
+
 
     handleUpdate = (event) => {
         event.preventDefault();
 
-        const { username, address, phonenumber, sex } = this.state;
+        const { _id, username, address, phonenumber, sex } = this.state;
 
-        const requestData = { username, address, phonenumber, sex };
+        const requestData = { _id, username, address, phonenumber, sex };
 
         axios
             .put("http://localhost:3001/users", requestData)
@@ -111,22 +124,45 @@ export class Form extends Component {
             this.setState({
                 updateMode: true,
             });
-            console.log("update mode");
         } else if (mode[0] === "add") {
             this.setState({
                 updateMode: false,
             });
-            console.log("Add mode");
         }
 
-        const { updateMode } = this.state;
-        console.log(updateMode);
     };
+
+    handleUserChange = (event) => {
+        let userObj = JSON.parse(event.target.value);
+
+        //const { username, address, phonenumber, sex } = this.state;
+        this.setState({
+            _id: userObj._id,
+            username: userObj.username,
+            phonenumber: userObj.phonenumber,
+            address: userObj.address,
+            sex: userObj.sex
+        });
+
+    }
+
+    handleUserDelete = (event) => {
+        event.preventDefault();
+
+        axios.delete("http://localhost:3001/users/" + this.state._id).then((res) => {
+            if (res) {
+                console.log(res);
+            }
+
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
 
     render() {
         const { username, address, phonenumber, sex, updateMode, users } = this.state;
         return (
-            <form onSubmit={this.handleSubmit}>
+            <FormGroup className="wrapper">
 
                 <Grid container spacing={2} direction="column" alignItems="center">
                     <Grid item>
@@ -136,53 +172,75 @@ export class Form extends Component {
                         </ToggleButtonGroup>
                     </Grid>
                 </Grid>
-            
-                <div>
-                    <select value="dinlaie" onChange={this.handleSexChange}>
-                        {users.map(user=>(
-                            <option value={user.username}>{user.username}</option>
-                        ))}
-                      
-                    </select>
-                </div>
+                {updateMode ?
+                    <FormControl >
+                        <div className="field">
+                        <FormLabel className="field_label">Select User : </FormLabel>
+                        <Select className="select_field"  onChange={this.handleUserChange}>
+                            {users.map(user => (
+                                <option key={user._id} value={JSON.stringify(user)}>{user.username}</option>
+                            ))}
 
-                <div>
-                    <label>User Name : </label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={this.handleUsernameChange}
-                    ></input>
-                </div>
-                <div>
-                    <label>Phone Number : </label>
-                    <input
-                        type="text"
-                        value={phonenumber}
-                        onChange={this.handlePhonenumberChange}
-                    ></input>
-                </div>
-                <div>
-                    <label>Address : </label>
-                    <textarea
+                        </Select >
+                        </div>
+                    </FormControl>
+                    :
+                    <FormControl >
+                        <div className="field">
+                        <FormLabel className="field_label">User Name : </FormLabel>
+                        <TextField
+                            type="text"
+                            value={username}
+                            onChange={this.handleUsernameChange}
+                        ></TextField>
+                        </div>
+                    </FormControl>
+                }
+
+
+                <FormControl>
+                    <div className="field">
+                        <FormLabel className="field_label">Phone Number : </FormLabel>
+                        <TextField
+                            placeholder="Phone Number"
+                            type="text"
+                            value={phonenumber}
+                            onChange={this.handlePhonenumberChange}
+                        ></TextField>
+                    </div>
+                </FormControl>
+                <FormControl>
+                    <div className="field">
+                    <FormLabel className="field_label">Address : </FormLabel>
+                    <TextField
+                        placeholder="Type your Address here"
                         value={address}
                         onChange={this.handleAddressChange}
-                    ></textarea>
-                </div>
-                <div>
-                    <label>Sex : </label>
-                    <select value={sex} onChange={this.handleSexChange}>
+                    ></TextField >
+                    </div>
+                </FormControl>
+                <FormControl >
+                    <div className="field">
+                    <FormLabel className="field_label">Sex : </FormLabel>
+                    <Select className="select_field" value={sex} onChange={this.handleSexChange}>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
-                    </select>
-                </div>
-                <button onClick={this.handleDelete}>Delete</button>
+                    </Select >
+                    </div>
+                </FormControl>
+
                 {updateMode ? (
-                    <button onClick={this.handleUpdate}>Update</button>
+                    <div>
+                        <Button className="main_buttons" variant="contained" color="secondary" onClick={this.handleUserDelete}>Delete</Button>
+                        <Button className="main_buttons" variant="contained" color="primary" onClick={this.handleUpdate}>Update</Button>
+                    </div>
                 ) : (
-                    <button onClick={this.onSubmit}>Add</button>
+                    <div>
+                        <Button className="main_buttons" variant="contained" color="primary" onClick={this.onSubmit}>Add</Button>
+                    </div>
+
                 )}
-            </form>
+            </FormGroup>
         );
     }
 }
